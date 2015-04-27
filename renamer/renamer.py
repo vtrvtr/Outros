acd5b3dc0da8438327e2ea878ac690f2deb7790f
Vitor Hugo <vtr@hotmail.com.br>
2015-04-27 20:21:06 -0300

    yielding unformatted strings from episodes

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -4,46 +4,69 @@ import logging
 from pathlib import Path
 
 
+# MAKE BACKUP FUNCTIONALITY
+
 def transverse_directory(directory):
     yield directory
     for sub in directory.iterdir():
         if sub.is_dir():
-            yield from transverse_directory(sub)
+            for subdirectory in transverse_directory(sub):
+                yield subdirectory
         else:
             yield sub
 
 
 def move_files(source, dest=None, extensions=[]):
-    dest = dest or source.parent
+    dest = dest or Path(source)
     for f in transverse_directory(Path(source)):
         if not f.is_dir():
             if f.suffix in extensions or len(extensions) == 0:
                 try:
                     shutil.move(str(f), str(dest))
+                    # print(str(f), str(dest))
                 except shutil.Error:
                     pass
 
 
-def rename(source, season='1', extensions=['.mkv', '.mp4', '.mov']):
-    i_episode = 1
+def rename(source, episode, extensions=['all']):
     for episode in transverse_directory(Path(source)):
         if not episode.is_dir():
-            if episode.suffix in extensions:
+            if episode.suffix in extensions or 'all' in extensions:
+                print episode.name
+                with open('{}_backup.txt'.format(source), 'a') as f:
+                    f.write('{}\n'.format(episode.name))
                 episode.rename(
-                    source / 'S0{}E{}{}.mkv'.format(season, '0' if i_episode < 10 else '', i_episode))
-                i_episode += 1
+                    Path(source) / '{}.mkv'.format(episode))
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Renames files properly for easy Sonarr recognition')
-    parser.add_argument('--move', '-m', help="Move all files with the EXTENSIONS from SOURCE to DEST \n default dest = source.parent", nargs='+')
     parser.add_argument(
-        '--rename', '-r', help="Rename all files in SOURCE folder, needs to provide the correct SEASON", nargs='+')
+        '--move', '-m', help="Move all files with the EXTENSIONS from SOURCE to DEST \n default dest = source", action='store_true')
+    parser.add_argument(
+        '--rename', '-r', help="Rename all files in SOURCE folder, needs to provide the correct SEASON", action='store_true')
     args = parser.parse_args()
-    
+    if args.move:
+        source = input('Where are the files now?\n')
+        dest = input(
+            'Where the files should be? \ntype ENTER for default folder\n')
+        dest = dest if dest is not '' else None
+        extensions = input(
+            'Extensions of files you want to move \n type nothing to default types \n')
+        extensions = extensions if extensions is not '' else [
+            '.mkv', '.mp4', '.mov']
+        move_files(source, dest, extensions)
+    elif args.rename:
+        source = input('Where are the files now?\n')
+        season = input('What\'s the season?\n type nothing to 1')
+        season = season if season is not '' else '1'
+        extensions = input(
+            'Extensions of files you want to move \n type nothing to default types \n')
+        extensions = extensions if extensions is not '' else [
+            '.mkv', '.mp4', '.mov']
+        rename(source, season, extensions)
 
+# if __name__ == '__main__':
+#     main()
 
-
-if __name__ == '__main__':
-    main()
--- renamer/tbdb.py
+++ renamer/tbdb.py
@@ -1,25 +1,34 @@
 import tvdb_api
 import sys
-sys.path.insert(0, 'E:\\code\outros\\renamer\\renamer.py')
+# sys.path.insert(0, 'E:\\code\outros\\renamer\\renamer.py')
 import renamer
 from pathlib import Path
 
 
+class Serie(object):
+
+    def __init__(self, series_name):
+        self.name = series_name
+        self.t = tvdb_api.Tvdb()
+
+    def get_episodes(self, seasons=None):
+        self.seasons = [seasons] or seasons
+        try:
+            show = self.t[self.name]
+            for season in show:
+                if None in self.seasons or season in self.seasons:
+                    for _, name in show[season].items():
+                        # yield str(name).split()[1].split('x')
+                        yield map(str, (name['dvd_season'], name['episodenumber'], name['episodename']))
 
 
-def get_episodes(show_name, seasons = None):
-    t = tvdb_api.Tvdb()
-    seasons = [seasons] or seasons
-    print seasons
-    try:
-        show = t[show_name]
-        for season in show:
-            if None in seasons or season in seasons: 
-                for number, name in show[season].items():
-                    yield str(name).split()[1].split('x')
-    except tvdb_api.tvdb_shownotfound:
-        raise
+        except tvdb_api.tvdb_shownotfound:
+            raise
 
 
-for epi in get_episodes('code geass'):
-    print 'S{}E{}'.format(epi[0], epi[1])
+
+
+a = Serie('Code geass')
+
+for epi in a.get_episodes(1):
+    print 'S{}{}E{}{} - {}'.format('0' if int(epi[0]) < 10 else '', epi[0], '0' if int(epi[1]) < 10 else '',epi[1], epi[2])
