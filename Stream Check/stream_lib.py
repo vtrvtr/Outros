9ab42553fac894e19a875e7715bbfcd1a8f3e90b
Vitor Hugo <vtr@hotmail.com.br>
2015-05-21 11:53:07 -0300

    fixed multiple files path problems

---
+++ Stream Check/stream_lib.py
@@ -0,0 +1,30 @@
+class Streams(object):  # Base stream class, you need to load the dictionary
+
+
+    def __init__(self, stream_dict=None):
+        if stream_dict is not None:
+            self.streams = stream_dict
+        else:
+            self.streams = {}
+
+    def addStream(self, game, stream_url):
+        try:
+            if stream_url not in self.streams[game.upper()]:
+                self.streams[game.upper()].append(stream_url)
+            else:
+                pass
+        except:
+            self.streams[game.upper()] = [stream_url]
+
+    def getStream(self, stream_url):
+        for v in self.streams.values():
+            if v == stream_url:
+                return v
+
+    def getAllStreams(self):
+        return self.streams
+
+    def getGameStreams(self, game):
+        return self.streams[game]
+
+
