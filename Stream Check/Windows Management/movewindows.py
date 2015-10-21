68257d8cf0b85f1ab7ad63d3e66ee2428c3d978e
Vitor Hugo <vtr@hotmail.com.br>
2015-10-21 17:47:14 -0200

    added change_monitor func

--- Stream Check/Windows Management/movewindows.py
+++ Stream Check/Windows Management/movewindows.py
@@ -2,10 +2,13 @@ import pywinauto
 import time
 
 def init_wizard(): 
-    pwa_app = pywinauto.application.Application()
-    w_handle = pywinauto.findwindows.find_windows(title=u'stdin', class_name='MediaPlayerClassicW')[0]
-    Wizard = pwa_app.window_(handle=w_handle)
-    return Wizard
+    try:
+        pwa_app = pywinauto.application.Application()
+        w_handle = pywinauto.findwindows.find_windows(title=u'stdin', class_name='MediaPlayerClassicW')[0]
+        Wizard = pwa_app.window_(handle=w_handle)
+        return Wizard
+    except IndexError:
+        raise 'App not find'
 
 
 def get_position(app):
@@ -15,14 +18,15 @@ def get_position(app):
 
 def check_monitor(y_pos):
     '''Returns True if window is in the first (up) monitor'''
-    return True if y_pos < 0 else False
+    return True if y_pos > 0 else False
 
-
-try:
+def change_monitor():
     window = init_wizard()
     x_pos, y_pos, width, height = get_position(window)
-    print(check_monitor(y_pos))
+    print(x_pos)
+    if check_monitor(y_pos):
+        window.MoveWindow(x=0, y=-1080, width=1920, height=1080)
+    else:
+        window.MoveWindow(x=0, y=0, width=1920, height=1080)
 
-    # window.MoveWindow(x=0, y=-1080, width=1920, height=1080)
-except Exception as e:
-    print(e)
+change_monitor()
