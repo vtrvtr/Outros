f12296a6078d4c62d98104368a0145ce5b0ecae2
Vitor Hugo <vtr@hotmail.com.br>
2015-12-17 08:02:26 -0200

    changed movewindows to a class

--- Stream Check/movewindows.py
+++ Stream Check/movewindows.py
@@ -2,53 +2,40 @@ import pywinauto
 import warnings
 
 
-def init_wizard():
-    try:
-        pwa_app = pywinauto.application.Application()
-        w_handle = pywinauto.findwindows.find_windows(
-            title=u'stdin', class_name='MediaPlayerClassicW')[0]
-        Wizard = pwa_app.window_(handle=w_handle)
-        return Wizard
-    except IndexError:
-        warnings.warn('App not find')
+class WindowsPosition():
+    def __init__(self):
+        try:
+            pwa_app = pywinauto.application.Application()
+            w_handle = pywinauto.findwindows.find_windows(
+                title=u'stdin', class_name='MediaPlayerClassicW')[0]
+            self.app = pwa_app.window_(handle=w_handle)
+            self.y_pos = self.app.Rectangle().top
+            self.x_pos = self.app.Rectangle().left
+            self.width = self.app.Rectangle().width()
+            self.height =self.app.Rectangle().height()
+        except IndexError:
+            warnings.warn('App not find')
 
+    def move(self, monitor='monitor1', new_width=1920, new_height=1080):
+        '''monitor to send the window to (def: monitor1)
+                monitor1 -> up
+                monitor2 -> down
+            width of the windows (def: 1920)
+            height of the windows (def: 1080)'''
+        monitors_pos = {'monitor1': -1080, 'monitor2': 0}
+        try:
+            self.app.MoveWindow(x=0, y=monitors_pos[monitor], width=new_width, height=new_height)
+        except AttributeError:
+            warnings.warn('No windows to move')
 
-
-def get_position(app):
-    pos = app.Rectangle()
-    return pos.left, pos.top, pos.width(), pos.height()
-
-
-def check_monitor(y_pos):
-    '''Returns True if window is in the first (up) monitor'''
-    return True if y_pos < -71 else False
-
-
-def change_monitor(app, monitor='monitor1', width=1920, height=1080):
-    '''monitor to send the window to (def: monitor1)
-            monitor1 -> up
-            monitor2 -> down
-        width of the windows (def: 1920)
-        height of the windows (def: 1080)'''
-    monitors_pos = {'monitor1': -1080, 'monitor2': 0}
-    app = init_wizard()
-    try:
-        app.MoveWindow(x=0, y=monitors_pos[monitor], width=1920, height=1080)
-    except AttributeError:
-        warnings.warn('No windows to move')
-
-
-
-def toggle_visibility(app, action='minimize'):
-    '''Minimizes or maximizes windows (def: minimize)
-        app: a pywinauto window with a handle
-        action: minimize or maximize'''
-    try:
-        if action == 'minimize':
-            app.Minimize()
-        elif action == 'maximize':
-            app.Maximize()
-    except AttributeError:
-        raise Exception("A pywinauto window_ is necessary")
-
-
+    def toggle_visibility(app, action='minimize'):
+        '''Minimizes or maximizes windows (def: minimize)
+            app: a pywinauto window with a handle
+            action: minimize or maximize'''
+        try:
+            if action.lower() == 'minimize':
+                self.app.Minimize()
+            elif action.lower() == 'maximize':
+                self.app.Maximize()
+        except AttributeError:
+            raise Exception("A pywinauto window_ is necessary")
